1db1072ee82b65cf85d7a8b0400da0a4ef420ac2
Prasad <lakshmiprasad1487@gmail.com>
2023-11-16 22:35:43 +0530

    commiting nested list

---
+++ Interview_Questions/nested_list_squere.py
@@ -0,0 +1,17 @@
+l = [[1,2,3],[4,5,6],[7,8,9]]
+
+import math
+
+def sq_li(x):
+    res_li = []
+
+    for i in x:
+        int_li = []
+        for j in i:
+            int_li.append(int(math.pow(j,2)))
+
+        res_li.append(int_li)
+
+    return res_li
+
+print(sq_li(l))
