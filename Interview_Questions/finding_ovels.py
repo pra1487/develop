8374ea136a8d9caf431ea068df118525fb6ac1d4
Prasad <lakshmiprasad1487@gmail.com>
2024-04-12 21:31:02 +0530

    spark configs.

--- Interview_Questions/finding_ovels.py
+++ Interview_Questions/finding_ovels.py
@@ -5,8 +5,11 @@ def vol_find(x):
     vol_l = []
 
     for i in x:
-        if i in 'aeiouAEIOU':
+        if i in 'aeiouAEIOU' and i not in vol_l:
             vol_l.append(i)
+
+    vol_l.sort()
+
     return vol_l
 
 print(vol_find(s))
