e150cfc1b2c789ddb9a495fc74725c2fd66de387
Prasad <lakshmiprasad1487@gmail.com>
2024-04-18 22:55:54 +0530

    spark configs.

--- spark/Dataframes/Spark_Practice.py
+++ spark/Dataframes/Spark_Practice.py
@@ -21,35 +21,14 @@
 """
 
 import pyspark
+import pandas as pd
+from urllib.request import urlopen
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-from urllib.request import urlopen
+from pyspark.sql.window import Window
 
 spark = SparkSession.builder.appName('practice').master('local[*]').getOrCreate()
 sc = spark.sparkContext
 sc.setLogLevel('error')
 
-simpleData = [("James", "Sales", 3000),
-    ("Michael", "Sales", 4600),
-    ("Robert", "Sales", 4100),
-    ("Maria", "Finance", 3000),
-    ("James", "Sales", 3000),
-    ("Scott", "Finance", 3300),
-    ("Jen", "Finance", 3900),
-    ("Jeff", "Marketing", 3000),
-    ("Kumar", "Marketing", 2000),
-    ("Saif", "Sales", 4100)
-  ]
-
-cols = 'name string, dept string, salary int'
-
-df = spark.createDataFrame(simpleData, cols)
-#df.show()
-df.createOrReplaceTempView('emp_table')
-
-df3 = df.groupby('dept').count().sort('count', ascending=False)
-#df3.show()
-
-print(sc.defaultParallelism)
-print(df.rdd.getNumPartitions())
