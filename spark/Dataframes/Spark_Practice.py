1baf0a4befe03a1fe2745a728da7c5dfd7477bf2
Prasad <lakshmiprasad1487@gmail.com>
2024-05-03 23:22:00 +0530

    spark configs.

--- spark/Dataframes/Spark_Practice.py
+++ spark/Dataframes/Spark_Practice.py
@@ -20,38 +20,13 @@
 19. window func, web api data process
 """
 
-import pyspark
-import pandas as pd
-from urllib.request import urlopen
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
+s = "This is prasad"
 
-spark = SparkSession.builder.appName('practice').master('local[*]').getOrCreate()
-sc = spark.sparkContext
-sc.setLogLevel('Error')
+def rw(x):
+    wl = x.split()
+    wl.reverse()
 
-data1 = [("James","Sales","NY",90000,34,10000), \
-    ("Michael","Sales","NY",86000,56,20000), \
-    ("Robert","Sales","CA",81000,30,23000), \
-    ("Maria","Finance","CA",90000,24,23000) \
-  ]
+    result = " ".join(wl)
+    return result
 
-data2 = [("James","Sales","NY",90000,34,10000), \
-    ("Maria","Finance","CA",90000,24,23000), \
-    ("Jen","Finance","NY",79000,53,15000), \
-    ("Jeff","Marketing","CA",80000,25,18000), \
-    ("Kumar","Marketing","NY",91000,50,21000) \
-  ]
-
-cols = 'emp_name string, depart string, state string, Salary long, age int, bonus int'
-
-
-df1 = spark.createDataFrame(data1, cols)
-df2 = spark.createDataFrame(data2, cols)
-
-union_df = df1.unionAll(df2)
-print(union_df.count())
-
-union_df1 = df1.union(df2)
-print(union_df1.count())
+print(rw(s))
