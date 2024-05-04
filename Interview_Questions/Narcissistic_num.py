06646a084a9755323cce29f4b4dc982418fd3af9
Prasad <lakshmiprasad1487@gmail.com>
2024-05-04 16:40:06 +0530

    spark configs.

--- Interview_Questions/Narcissistic_num.py
+++ Interview_Questions/Narcissistic_num.py
@@ -2,12 +2,15 @@ x = 1234
 import math
 
 def func(x):
+    """ check the number is Narcissistic or not"""
     pow = len(str(x))
     sum = 0
     for i in str(x):
         sum = sum+(math.pow(int(i),pow))
 
-    result = sum
-    return sum
+    if (x==sum):
+        print(f"Yes {x} = {sum}")
+    else:
+        print(f"No {x} != {sum}")
 
-print(func(x))
+func(1634)
---
+++ Interview_Questions/count_lower_upper_string.py
@@ -0,0 +1,17 @@
+x = "Hellow world This Python IS VERY gooD For CAREEr"
+
+def count(x):
+
+    up_cnt = 0
+    lw_cnt = 0
+
+    for i in x:
+        if i.isupper()==True:
+            up_cnt += 1
+        elif (i.islower()==True):
+            lw_cnt += 1
+
+    return up_cnt, lw_cnt
+
+x,y = count(x)
+print(f"Upper count is: {x}")
