0d6bb691256bf4eb3bd5a1561483a901a0cb002f
Prasad <lakshmiprasad1487@gmail.com>
2024-05-04 23:46:10 +0530

    spark configs.

--- spark/Dataframes/Spark_Practice.py
+++ spark/Dataframes/Spark_Practice.py
@@ -20,7 +20,19 @@
 19. window func, web api data process
 """
 
+li = ['dhoni200', 'pandya40','jaddu50','virat10','rahul30']
 
+def sort_li(x):
+    rl = []
+    for i in x:
+        c = ""
+        for j in i:
+            if(j.isdigit()):
+                c += j
+        rl.append([int(c), i])
+    rl.sort(key=lambda x: x[0])
+    return [x[1] for x in rl]
+print(sort_li(li))
 
 
 
