dbf960051106e070a576216a876e853dca46cef6
Prasad <lakshmiprasad1487@gmail.com>
2024-05-11 16:14:06 +0530

    spark configs.

---
+++ Azure_databricks/Databricks_mount_unmount.py
@@ -0,0 +1,13 @@
+"""
+Mount and UnMount:
+====================
+
+mount()
+--------        dbutils.fs.help('mount')
+
+storage account name : pp_storage_dev
+container name : input_datasets
+files: orders.csv, customer.csv
+
+
+"""
