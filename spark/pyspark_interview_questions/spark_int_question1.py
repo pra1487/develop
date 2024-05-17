3196d6ef618a7a7c92f509d3713336bea02a8513
Prasad <lakshmiprasad1487@gmail.com>
2024-05-17 22:50:37 +0530

    spark configs.

--- spark/pyspark_interview_questions/spark_int_question1.py
+++ spark/pyspark_interview_questions/spark_int_question1.py
@@ -15,14 +15,15 @@ order_data = [(1,5),(2,6),(3,5),(3,6),(1,6)]
 order_cols = "customer_id int, product_key int"
 
 product_df = spark.createDataFrame(prod_data, prod_cols)
-product_df.show()
+#product_df.show()
 
 order_df = spark.createDataFrame(order_data, order_cols)
-order_df.show()
+#order_df.show()
 
 dist_product = product_df.agg(countDistinct(col('product_key')).alias('cnt_products'))
+dist_product.show()
 dist_orders = order_df.groupby('customer_id').agg(countDistinct(col('product_key')).alias('cnt_products'))
-
+dist_orders.show()
 df = dist_product.join(dist_orders, dist_product.cnt_products==dist_orders.cnt_products, 'inner').select(dist_orders.customer_id)
 df.show()
 
