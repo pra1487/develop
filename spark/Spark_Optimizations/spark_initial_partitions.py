3a1cbf64842c17e1709475673b5b189dcbddc0c9
Prasad <lakshmiprasad1487@gmail.com>
2024-06-22 15:42:15 +0530

    spark configs.

--- spark/Spark_Optimizations/spark_initial_partitions.py
+++ spark/Spark_Optimizations/spark_initial_partitions.py
@@ -14,7 +14,7 @@ let's consider scenario-1:
         - we have 1.1gb of file
         - we have 2 executors with 1gb RAM and 1 CPU core.
     Now partition size will be min(128mb, 1.1gb/2) = min(9, 2)
-    So, now spaek will create 9 number of initial partitions while creating the dataframe on 1.1gb dof data file
+    So, now spark will create 9 number of initial partitions while creating the dataframe on 1.1gb dof data file
 
 spark.sql.files.maxPartitionsBytes = 128m
 
