a91add078f6e46bc2bb6dc7910aabaedd5fa2e72
Prasad <lakshmiprasad1487@gmail.com>
2024-09-28 18:45:21 +0530

    spark configs.

--- spark/Dataframes/spark_distinct_drop_dups.py
+++ spark/Dataframes/spark_distinct_drop_dups.py
@@ -7,16 +7,16 @@ spark = SparkSession.builder.appName('inter Quest').master('local[*]').getOrCrea
 sc = spark.sparkContext
 sc.setLogLevel('Error')
 
-data = [("James", "Sales", 3000), \
-    ("Michael", "Sales", 4600), \
-    ("Robert", "Sales", 4100), \
-    ("Maria", "Finance", 3000), \
-    ("James", "Sales", 3000), \
-    ("Scott", "Finance", 3300), \
-    ("Jen", "Finance", 3900), \
-    ("Jeff", "Marketing", 3000), \
-    ("Kumar", "Marketing", 2000), \
-    ("Saif", "Sales", 4100) \
+data = [("James", "Sales", 3000),
+    ("Michael", "Sales", 4600),
+    ("Robert", "Sales", 4100),
+    ("Maria", "Finance", 3000),
+    ("James", "Sales", 3000),
+    ("Scott", "Finance", 3300),
+    ("Jen", "Finance", 3900),
+    ("Jeff", "Marketing", 3000),
+    ("Kumar", "Marketing", 2000),
+    ("Saif", "Sales", 4100)
   ]
 
 '''
