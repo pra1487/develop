3767a915343be77fabf0bc988a0fac81cf5d82c0
Prasad <lakshmiprasad1487@gmail.com>
2024-11-21 22:56:59 +0530

    Practice changed

--- spark/Dataframes/Spark_Practice.py
+++ spark/Dataframes/Spark_Practice.py
@@ -2,18 +2,7 @@ import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-from urllib.request import urlopen
+from pyspark.sql.window import Window
 
-spark = SparkSession.builder.master('local[*]').appName('practice').getOrCreate()
-sc = spark.sparkContext
-sc.setLogLevel('Error')
+spark = SparkSession.builder.appName('practice').master('local[*]').getOrCreate()
 
-data = [(1,"20200828"),(2,"20180525")]
-columns=["id","date"]
-
-df = spark.createDataFrame(data, columns)
-df.show()
-
-final_df = df.select('id', substring(col('date'), 1,4).alias('Year'),
-                     substring(col('date'),5,2).alias('Month'),\
-                     substring())
