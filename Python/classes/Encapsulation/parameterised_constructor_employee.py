6c668ab0f49be0f167198dcce4f2328f5ce75871
Prasad <lakshmiprasad1487@gmail.com>
2024-11-23 10:39:06 +0530

    cube volume with constructor class

---
+++ Python/classes/Encapsulation/parameterised_constructor_employee.py
@@ -0,0 +1,18 @@
+class employe:
+    """Employee Application"""
+    company = 'Cognizant'
+    empcount = 0
+
+    def __init__(self, empname, empid, empsal, design='SE', *languages, **details):
+        self.empname = empname
+        self.empid = empid
+        self.empsal = empsal
+        self.design = design
+        self.languages = languages
+        self.details = details
+        employe.empcount = employe.empcount+1
+    def display(self):
+        print('Name=',self.empname,'ID=',self.empid, 'Salary=',self.empsal,'Langs=',self.languages,'Details=',self.details)
+
+emp1 = employe('Virat',101,12000,'SA','ENG','HINDI', age=37, city='BANG')
+emp1.display()
