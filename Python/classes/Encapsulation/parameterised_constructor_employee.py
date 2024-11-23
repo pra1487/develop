c7bc6a63b59286f0a260b6b9a7a0e9cbba22b377
Prasad <lakshmiprasad1487@gmail.com>
2024-11-23 10:48:55 +0530

    cube volume with constructor class

--- Python/classes/Encapsulation/parameterised_constructor_employee.py
+++ Python/classes/Encapsulation/parameterised_constructor_employee.py
@@ -15,4 +15,10 @@ class employe:
         print('Name=',self.empname,'\nID=',self.empid,'\nSalary=',self.empsal,'\nLangs=',self.languages,'\nDetails=',self.details,)
 
 emp1 = employe('Virat',101,12000,'SA','ENG','HINDI', age=37, city='BANG')
-emp1.display()
+emp2 = employe('Rohit',102,11000,'SA','ENG','HINDI', age=38, city='MUMB')
+emp3 = employe('Rahul',103,10000,'SA','ENG','HINDI', age=30, city='LUCK')
+emp3.display()
+
+x = [emp1, emp2, emp3]
+for i in x:
+    print(i.display())
