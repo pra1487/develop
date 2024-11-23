572ba3c67ee1d7f13295f5723eefa297c4617a18
Prasad <lakshmiprasad1487@gmail.com>
2024-11-23 10:29:50 +0530

    cube volume with constructor class

--- Python/classes/Encapsulation/constructor_Arbitrary_args.py
+++ Python/classes/Encapsulation/constructor_Arbitrary_args.py
@@ -2,7 +2,7 @@ class sample:
     def __init__(self, *x, **y):
         self.x = x
         self.y = y
-        print('x=',self.x, type(x))
-        print('y=',self.y, type(y))
+        print('x=',self.x, type(x),len(x))
+        print('y=',self.y, type(y),len(x))
 
 obj1 = sample(10,20,30, age=30, name='krish')
