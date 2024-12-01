951fa6b2e95f6d4e7c273894380228164bf8b037
Prasad <lakshmiprasad1487@gmail.com>
2024-12-01 11:47:49 +0530

    cube volume with constructor class

--- spark/Dataframes/Spark_Practice.py
+++ spark/Dataframes/Spark_Practice.py
@@ -3,24 +3,15 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from pyspark.sql.window import *
+from urllib.request import urlopen
 
 spark = SparkSession.builder.appName('practice').master('local[*]').getOrCreate()
 
-path = 'file:///D://data/txns.txt'
-
-data=[(1,'2024-01-01',"I1",10,1000),(2,"2024-01-15","I2",20,2000),(3,"2024-02-01","I3",10,1500),
-      (4,"2024-02-15","I4",20,2500),(5,"2024-03-01","I5",30,3000),(6,"2024-03-10","I6",40,3500),
-      (7,"2024-03-20","I7",20,2500),(8,"2024-03-30","I8",10,1000)]
-schema=["SOId","SODate","ItemId","ItemQty","ItemValue"]
-
-df = spark.createDataFrame(data, schema)
+rdd1 = spark.sparkContext.textFile('file:///D://data/hello.txt')
+rdd2 = rdd1.flatMap(lambda x: x.split(' '))
+rdd3 = rdd2.map(lambda x: (x,1))
+rdd4 = rdd3.reduceByKey(lambda x,y: x+y)
+rdd5 = rdd4.repartition(4)
+print(rdd5.getNumPartitions())
+df = spark.createDataFrame(rdd5, schema='name string, count int')
 df.show()
-
-df1 = df.withColumn('SODate', to_date(col('SODate'), 'yyyy-MM-dd'))\
-       .select('SOId', month(col('SODate')).alias('Month'), year(col('SODate')).alias('Year'), 'ItemValue')
-df1.show()
-
-df2 = df1.groupBy('Month', 'Year').agg(sum('ItemValue').alias('Total_sales'))\
-    .withColumn('Prev_sal', lag(col('Total_sales')).over(Window.orderBy('Month')))\
-    .withColumn('sales_percentage', (col('Total_sales')-col('Prev_sal'))/col('Total_sales')*100)
-df2.show()
--- spark/pyspark_interview_questions/RDD_word_count.py
+++ spark/pyspark_interview_questions/RDD_word_count.py
@@ -4,8 +4,12 @@ from pyspark.sql.functions import *
 
 spark = SparkSession.builder.appName('word_count').master('local[*]').getOrCreate()
 
-rdd1 = spark.sparkContext.textFile("file:///D://data/hello.txt")
-rdd2 = rdd1.flatMap(lambda x: x.split(" "))
+rdd1 = spark.sparkContext.textFile('file:///D://data/hello.txt')
+rdd2 = rdd1.flatMap(lambda x: x.split(' '))
 rdd3 = rdd2.map(lambda x: (x,1))
 rdd4 = rdd3.reduceByKey(lambda x,y: x+y)
-rdd4.saveAsTextFile('file:///D://data/Nov242024/')
+rdd5 = rdd4.repartition(4)
+print(rdd5.getNumPartitions())
+df = spark.createDataFrame(rdd5, schema='name string, count int')
+df.show()
+
