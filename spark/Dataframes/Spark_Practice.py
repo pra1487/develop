cedaf52b83b8cbd79bff5487b2d63a35c4e8d21a
Prasad <lakshmiprasad1487@gmail.com>
2024-12-14 22:38:51 +0530

    Multi Array Json Read.

--- spark/Dataframes/Spark_Practice.py
+++ spark/Dataframes/Spark_Practice.py
@@ -1,22 +1,12 @@
 import pyspark
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
 from pyspark.sql.types import *
+from pyspark.sql.functions import *
 from pyspark.sql.window import *
 from urllib.request import urlopen
 
 spark = SparkSession.builder.appName('practice').master('local[*]').getOrCreate()
 
-prod_data = [(5,), (6,)]
-prod_cols = "product_key int"
-prod_df = spark.createDataFrame(prod_data, prod_cols)
-
-
-
-
-
-
-
-
-
-
+df = spark.read.json('file:///D://data/ComplexData/MultiArrays1.json', multiLine=True)
+df = df.withColumn('Students', explode(col('Students')))
+df = df.withColumn('')
