90d43f8e8778fb79c5acc6482985fad15bd820aa
Prasad <lakshmiprasad1487@gmail.com>
2024-12-14 23:00:24 +0530

    Multi Array Json Read.

--- spark/Dataframes/Spark_Practice.py
+++ spark/Dataframes/Spark_Practice.py
@@ -7,6 +7,7 @@ from urllib.request import urlopen
 
 spark = SparkSession.builder.appName('practice').master('local[*]').getOrCreate()
 
-df = spark.read.json('file:///D://data/ComplexData/MultiArrays1.json', multiLine=True)
-df = df.withColumn('Students', explode(col('Students')))
-df = df.withColumn('')
+from datetime import date
+
+cycl_id = "".join(str(date.today()).split('-'))+'000000'
+print(f'cycl_id: {cycl_id}')
