7d5075a43418884504b255b7fb8231fe7d814447
Prasad <lakshmiprasad1487@gmail.com>
2025-01-25 07:53:35 +0530

    Practice...

--- spark/Dataframes/Spark_Practice.py
+++ spark/Dataframes/Spark_Practice.py
@@ -1,13 +1,24 @@
 import pyspark
 from pyspark.sql import SparkSession
-from pyspark.sql.types import *
 from pyspark.sql.functions import *
-from pyspark.sql.window import *
+from pyspark.sql.types import *
+
+from datetime import date
 from urllib.request import urlopen
 
 spark = SparkSession.builder.appName('practice').master('local[*]').getOrCreate()
 
-from datetime import date
+url = "https://randomuser.me/api/0.8/?results=200"
 
-cycl_id = "".join(str(date.today()).split('-'))+'000000'
-print(f'cycl_id: {cycl_id}')
+url_open = urlopen(url).read().decode('utf-8')
+url_rdd = spark.sparkContext.parallelize([url_open])
+url_df = spark.read.json(url_rdd)
+url_df.printSchema()
+
+url_df = url_df.withColumn('results', explode(col('results')))
+
+url_df = url_df.select(col('results.user.name.first').alias('first_name'),
+                       col('results.user.name.last').alias('last_name'),
+                       col('results.user.phone').alias('phone'),
+                       col('results.user.email').alias('mail_id'))
+url_df.show(truncate=True)
