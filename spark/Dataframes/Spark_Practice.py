166cdd3fcf43eb090a62398bb609fbde4dade97f
Prasad <lakshmiprasad1487@gmail.com>
2025-02-15 21:56:16 +0530

    Practice...

--- spark/Dataframes/Spark_Practice.py
+++ spark/Dataframes/Spark_Practice.py
@@ -1,24 +1,14 @@
-import pyspark
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
 from pyspark.sql.types import *
-from urllib.request import urlopen
-from datetime import date
+from pyspark.sql.functions import *
 from pyspark.sql.window import Window
+from datetime import date
+from urllib.request import urlopen
 
 spark = SparkSession.builder.master('local[*]').appName('practice').getOrCreate()
 sc = spark.sparkContext
-sc.setLogLevel('Error')
+sc.setLogLevel('error')
 
-data = [('IT', 'M'),('IT', 'F'),('IT', 'M'),('IT', 'M'),
-        ('HR', 'F'),('HR', 'M'),('HR', 'F'),('HR', 'F'),('HR', 'M'),
-        ('Sales', 'M'),('Sales', 'F'),('Sales', 'M'),('Sales', 'F'),('Sales', 'M'),('Sales', 'M')]
-cols = ['DeptName', 'Gender']
+path = 'file:///D://data/book1.csv'
+path1 = 'file:///D://data/txns.txt'
 
-df = spark.createDataFrame(data, cols)
-df.show()
-
-df.createOrReplaceTempView('df')
-spark.sql("""select DeptName, count(*) as total_emp_count, sum(female) as female_count, sum(male) as male_count
-          from (select *, case when Gender = 'M' then 1 else 0 end as male, case when Gender = 'F' then 1 else 0 end as female
-          from df) group by DeptName""").show()
